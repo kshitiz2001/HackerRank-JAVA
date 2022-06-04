f10989d17b95132290493a02d94a6d4d02344e82
Kshitiz Kumar Singh <kshitiz.singh_cs20@gla.ac.in>
2022-06-04 09:45:26 +0530

    Java Stdin and Stdout II

--- Java_Stdin_and_Stdout_II.java
+++ Java_Stdin_and_Stdout_II.java
@@ -1,4 +1,4 @@
-"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""Java Stdin and Stdout II"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+/*"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""Java Stdin and Stdout II"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
 
 
 In this challenge, you must read an integer, a double, and a String from stdin, then print the values according to the instructions in the Output Format section below. To make the problem a little easier, a portion of the code is provided for you in the editor.
@@ -33,12 +33,12 @@ Welcome to HackerRank's Java tutorials!
 Sample Output
 String: Welcome to HackerRank's Java tutorials!
 Double: 3.1415
-Int: 42
+Int: 42*/
 
 
 
 
-**SOLUTION**
+//**SOLUTION**//
 
 
 import java.util.Scanner;
